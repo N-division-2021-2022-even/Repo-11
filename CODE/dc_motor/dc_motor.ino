7973fdfac763d398d728a52b26c5d7586a2f1b14
psbhat007 <psbhat007@gmail.com>
2022-07-26 22:31:40 +0530

    updated

--- CODE/dc_motor/dc_motor.ino
+++ CODE/dc_motor/dc_motor.ino
@@ -9,13 +9,11 @@ void setup() {
 void loop() {
   digitalWrite(11,HIGH);
   digitalWrite(12,LOW);
-  delay(30000);
+  delay(3000);
   digitalWrite(11,LOW);
   digitalWrite(12,HIGH);
-  delay(20000);
-  digitalWrite(11,LOW);
-  digitalWrite(12,LOW);
-  delay(2000);
+  delay(3000);
+
   // put your main code here, to run repeatedly:
 
 }
